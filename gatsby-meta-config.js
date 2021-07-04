facef923d77eed3fb594f397cfeefb4e7585252c
jinu.noh <jinu.noh@bagelcode.com>
2021-07-04 19:51:58 +0900

    add comment module

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -16,7 +16,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'yesjinu/yesjinu.github.io', // Your repository for archive comment
+    utterances: 'yesjinu/yes-jinu.com', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
